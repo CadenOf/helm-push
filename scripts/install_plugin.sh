5f54d7f9d3cc2b7ce5c757c3fdea796383d0bd76
System Administrator <root@CadendeMacBook-Pro.local>
2019-05-20 18:43:53 +0800

    [update] suport local installed

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -8,13 +8,23 @@ if [ -n "${HELM_PUSH_PLUGIN_NO_INSTALL_HOOK}" ]; then
 fi
 
 version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
-echo "Downloading and installing helm-push v${version} ..."
+#echo "Downloading and installing helm-push v${version} ..."
 
+#url=""
+#if [ "$(uname)" = "Darwin" ]; then
+#    url="https://github.com/chartmuseum/helm-push/releases/download/v${version}/helm-push_${version}_darwin_amd64.tar.gz"
+#elif [ "$(uname)" = "Linux" ] ; then
+#    url="https://github.com/chartmuseum/helm-push/releases/download/v${version}/helm-push_${version}_linux_amd64.tar.gz"
+#else
+#    url="https://github.com/chartmuseum/helm-push/releases/download/v${version}/helm-push_${version}_windows_amd64.tar.gz"
+#fi
+
+echo "Local installing helm-push v${version} ..."
 url=""
 if [ "$(uname)" = "Darwin" ]; then
     url="https://github.com/chartmuseum/helm-push/releases/download/v${version}/helm-push_${version}_darwin_amd64.tar.gz"
 elif [ "$(uname)" = "Linux" ] ; then
-    url="https://github.com/chartmuseum/helm-push/releases/download/v${version}/helm-push_${version}_linux_amd64.tar.gz"
+    url="./helm-push/dl/helm-push_${version}_linux_amd64.tar.gz"
 else
     url="https://github.com/chartmuseum/helm-push/releases/download/v${version}/helm-push_${version}_windows_amd64.tar.gz"
 fi
@@ -25,11 +35,12 @@ mkdir -p "bin"
 mkdir -p "releases/v${version}"
 
 # Download with curl if possible.
-if [ -x "$(which curl 2>/dev/null)" ]; then
-    curl -sSL "${url}" -o "releases/v${version}.tar.gz"
-else
-    wget -q "${url}" -O "releases/v${version}.tar.gz"
-fi
+#if [ -x "$(which curl 2>/dev/null)" ]; then
+#    curl -sSL "${url}" -o "releases/v${version}.tar.gz"
+#else
+#    wget -q "${url}" -O "releases/v${version}.tar.gz"
+#fi
+cp dl/helm-push_${version}_linux_amd64.tar.gz releases/v${version}.tar.gz
 tar xzf "releases/v${version}.tar.gz" -C "releases/v${version}"
 mv "releases/v${version}/bin/helmpush" "bin/helmpush" || \
     mv "releases/v${version}/bin/helmpush.exe" "bin/helmpush"
